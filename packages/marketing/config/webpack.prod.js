45659e7c89878c9557a0f53fb1ec076c9d90433e
Anand Sutar <AnandS7@citiustech.com>
2023-02-02 14:25:55 +0530

    Marketing deployment added

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,21 +1,22 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const prodConfig = {
-    mode: "production",
-    output: '[name].[contenthash].js',
-    plugins: [
-        new ModuleFederationPlugin({
-          name: 'marketing',
-          filename: 'remoteEntry.js',
-          exposes: {
-            './MarketingApp': './src/bootstrap',
-          },
-          shared: packageJson.dependencies,
-        }),
-      ],
-}
+	mode: "production",
+	output: "[name].[contenthash].js",
+	publicPath: "/marketing/latest/",
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "marketing",
+			filename: "remoteEntry.js",
+			exposes: {
+				"./MarketingApp": "./src/bootstrap",
+			},
+			shared: packageJson.dependencies,
+		}),
+	],
+};
 
 module.exports = merge(commonConfig, prodConfig);
